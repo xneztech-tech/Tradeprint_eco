8e8b101be24936a3f7d062a8b56142399993c757
xneztech-tech <xneztech@gmail.com>
2025-12-19 08:58:47 +0530

    Update settings.py

--- tradeprint_project/settings.py
+++ tradeprint_project/settings.py
@@ -84,10 +84,17 @@ WSGI_APPLICATION = 'tradeprint_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+#         conn_max_age=600
+#     )
+# }
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
     )
 }
 # DATABASES = {
